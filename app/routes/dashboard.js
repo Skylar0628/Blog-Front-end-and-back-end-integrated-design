cb30695ad5f5247a7734447f8f665808b92e554a
user <a0903131602@gmail.com>
2024-07-25 19:56:36 +0800

    製作後台文章分類 _修改

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,55 +1,54 @@
 var express = require('express');
 var router = express.Router();
-const { db,adminAuth} = require('../connections/firebase_admin_connect');
+const {db} = require('../connections/firebase_admin_connect');
 
-const categoriesRef = db.ref('categories'); //分類路徑
+const categoriesRef = db.ref('categories');
+
 
 // 檔案頁面
 router.get('/archives', function(req, res, next) {
-  res.render('dashboard/archives', { title: 'Express' });
-});
-
-// 文章頁面
-router.get('/article', function(req, res, next) {
-  res.render('dashboard/article', { title: 'Express' });
-});
-
-// 分類頁面
-router.get('/categories', function(req, res, next) {
-  res.render('dashboard/categories', { title: 'Express' });
-});
-
-// 分類頁面
-router.get('/categories', function(req, res, next) {
-  res.render('dashboard/categories', { title: 'Express' });
-});
-
-// 註冊頁面
-router.get('/signup', function(req, res, next) {
-  res.render('dashboard/signup', { title: 'Express' });
-});
-
-////////////////////////////////////////////////////////////////////////
-
-// 新增分類
-router.post('/categories/create',(req, res)=>{
- const data = req.body;
- const plainData = {...data};
- const categoryRef = categoriesRef.push();
- const key = categoryRef.key;
- plainData.id =key;
- categoryRef.set(plainData)
- .then((result)=>{
-    console.log(result);
-    res.redirect('/dashboard/categories');
- })
- .catch((err)=>{
-    console.log(err);
-    res.redirect('/dashboard/categories');
- });
-});
+    res.render('dashboard/archives', { title: 'Express' });
+  });
+  
+  // 文章頁面
+  router.get('/article', function(req, res, next) {
+    res.render('dashboard/article', { title: 'Express' });
+  });
+  
+  // 分類頁面
+  router.get('/categories', function(req, res, next) {
+    res.render('dashboard/categories', { title: 'Express' });
+  });
+  
+  // 分類頁面
+  router.get('/categories', function(req, res, next) {
+    res.render('dashboard/categories', { title: 'Express' });
+  });
+  
+  // 註冊頁面
+  router.get('/signup', function(req, res, next) {
+    res.render('dashboard/signup', { title: 'Express' });
+  });
 
 
+  // 新增分類
+  router.post('/categories/create', function(req, res, next) {
+    const data = req.body;
+    const plainData = {...data}
+    const category = categoriesRef.push();
+    const key = category.key;
+    plainData.id = key;
 
+    category.set(plainData)
+    .then((result)=>{
+        console.log(result);
+        res.redirect('/dashboard/categories');
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.redirect('/dashboard/categories');
+    });
+  });
+  
 
 module.exports = router;
